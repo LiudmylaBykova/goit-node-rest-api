9f3c07cb3921575b22151869e68147c7f69538cd
LiudmylaBykova <bykova.luda@gmail.com>
2024-05-16 12:49:27 +0300

    add function validate id

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -15,19 +15,27 @@ import {
   updateStatusSchema,
 } from "../schemas/contactSchemas.js";
 
+import isValidId from "../helpers/isValidId.js";
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", isValidId, getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", isValidId, deleteContact);
 
 contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(updateContactSchema),
+  updateContact
+);
 contactsRouter.patch(
   "/:id/favorite",
+  isValidId,
   validateBody(updateStatusSchema),
   updateStatusContact
 );
